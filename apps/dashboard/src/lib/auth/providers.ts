934e70dbd9d60f3f3aa765e4adf6c9983332e759
Jonah <54612613+jonahships@users.noreply.github.com>
2026-04-01 20:48:08 -0400

    Update providers.ts

--- apps/dashboard/src/lib/auth/providers.ts
+++ apps/dashboard/src/lib/auth/providers.ts
@@ -10,19 +10,12 @@ export const GoogleProvider = Google({
   allowDangerousEmailAccountLinking: true,
   authorization: {
     params: {
-      // See https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
       prompt: "select_account",
-      // scope:
-      //   "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
     },
   },
 });
 
 export const ResendProvider = Resend({
-  apiKey: undefined, // REMINDER: keep undefined to avoid sending emails
-  async sendVerificationRequest(params) {
-    console.log("");
-    console.log(`>>> Magic Link: ${params.url}`);
-    console.log("");
-  },
+  apiKey: process.env.RESEND_API_KEY,
+  from: "PipJuice Status <status@pipjuice.io>",
 });
