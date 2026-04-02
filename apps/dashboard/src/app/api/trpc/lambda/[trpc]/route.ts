1aee462743bf57efb9e56bef9aee7ed5829e58d2
Jonah <54612613+jonahships@users.noreply.github.com>
2026-04-01 21:10:09 -0400

    Update tRPC handler to improve context creation
    Refactor createContext to include auth function.

--- apps/dashboard/src/app/api/trpc/lambda/[trpc]/route.ts
+++ apps/dashboard/src/app/api/trpc/lambda/[trpc]/route.ts
@@ -5,15 +5,16 @@ import { auth } from "@/lib/auth";
 import { createTRPCContext } from "@openstatus/api";
 import { lambdaRouter } from "@openstatus/api/src/lambda";
 
-// Stripe is incompatible with Edge runtimes due to using Node.js events
-// export const runtime = "edge";
-
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: "/api/trpc/lambda",
     router: lambdaRouter,
-    req: req,
-    createContext: () => createTRPCContext({ req, auth }),
+    req,
+    createContext: () =>
+      createTRPCContext({
+        req,
+        auth: () => auth(req),
+      }),
     onError: ({ error }) => {
       console.log("Error in tRPC handler (lambda)");
       console.error(error);
